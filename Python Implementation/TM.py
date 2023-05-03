8dfaeb52e233349a86981c29fe88bc449e5a93b8
Sage Allen <allen.sage4@gmail.com>
2023-05-03 18:30:21 +0800

    updated version

--- Python Implementation/TM.py
+++ Python Implementation/TM.py
@@ -1,36 +1,60 @@
 import json
+import random
 import placeholderQB as QB
-
 
 JSON_FILENAME = "student_info.json"
 
 
-
-
 def main():
+    # main loop
+    # initialise html interface
+    # wait for response
+    printStudentSummary()
 
+
+class Test:
+    session_ids = []
+
+    def __init__(self, num_questions):
+        self.questions = []
+        self.session_id
+
+        # create a session id to keep track of the test
+        if len(session_ids) == 0:
+            self.session_id = 0
+        else:
+            self.session_id = session_ids[-1] +1
+
+
+def printStudentSummary():
     with open(JSON_FILENAME, "r") as file:
         json_data = json.load(file)
 
-        for i in json_data:
-            print(i, json_data[i])
+        print("Student Summary")
+        for student_id, student in json_data.items():
+            print(student_id, "->", student)
 
 
-def get_question():
-    # gets a question to give to 
-    pass
+def get_question(question_bank):
+    question = question_bank.get_question()
+    return question
+    
+    # display question to 
 
-def mark_question(question):
-    pass
+
+def mark_question(question_bank, answer):
+    correct = question_bank.mark_question()
+
+    if correct:
+        # adjust mark as necessary
+
+        pass
 
 
 
 if __name__ == "__main__":
-
     # create QB object
 
     QB1 = QB.QB()
 
     main()
-
-
--- Python Implementation/placeholderQB.py
+++ Python Implementation/placeholderQB.py
@@ -5,58 +5,66 @@ import random
 
 QUESTION_FILENAME = "questions.csv"
 
+
 class QB:
-	def __init__(self):
-		print("instantiated a QB object {}".format(id(self)))
+    def __init__(self):
+        print("instantiated a QB object {}".format(id(self)))
 
-		self.question_bank = {}
-		self.num_questions = 0
+        self.question_bank = {}
+        self.num_questions = 0
 
-		# initiate questions
-		with open(QUESTION_FILENAME, newline='') as csvfile:
-			reader = csv.reader(csvfile, delimiter=',')
+        # initiate questions
+        with open(QUESTION_FILENAME, newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
 
-			id_count = 0
-			for row in reader:
-				question = row[0]
-				options = row[1:5]
-				answer = row[5]
+            id_count = 0
+            for row in reader:
+                question = row[0]
+                options = row[1:5]
+                answer = row[5]
 
-				self.question_bank[id_count] = {
-					"question": question,
-					"options": options,
-					"answer": answer
-				}
+                self.question_bank[id_count] = {
+                    "question": question,
+                    "options": options,
+                    "answer": answer
+                }
 
-				id_count += 1
+                id_count += 1
 
-			self.num_questions = id_count
-			if self.num_questions == 0:
-				raise Exception("Warning, zero questions")
+            self.num_questions = id_count
+            if self.num_questions == 0:
+                raise Exception("Warning, zero questions")
 
+    def sendQuestion(self):
+        rand_id = random.randint(0, self.num_questions - 1)
 
-	def sendQuestion(self):
-		rand_id = random.randint(0, self.num_questions-1)
+        return {
+            "id": rand_id,
+            "question": self.question_bank[rand_id]["question"],
+            "options": self.question_bank[rand_id]["options"]
+        }
 
-		return {
-			"id": rand_id,
-			"question": self.question_bank[rand_id]["question"],
-			"options": self.question_bank[rand_id]["options"]
-		}
-		
-
-	def markQuestion(self, question_id, answer):
-		try:
-			if answer == self.question_bank[question_id][answer]:
-				return True
-		catch
-	
-
+    def markQuestion(self, question_id, answer):
+        try:
+            if answer == self.question_bank[question_id][answer]:
+                return True
+        except KeyError:
+            return False
 
 
 if __name__ == "__main__":
-	qb1 = QB()
+    qb1 = QB()
 
-	print(qb1.sendQuestion())
-	print(qb1.markQuestion(0, "Seattle"))
+    print(qb1.sendQuestion())
+    print(qb1.markQuestion(0, "Seattle"))
+
+
+
+
+
+
+
+
+
+
 
