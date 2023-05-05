caff713e373ddc5f37ff638b20b83f30b185f52b
owenls <owen@wiredfamily.org>
2023-05-05 18:17:27 +0800

    checksum not done properly right now

--- TCP/QB-before.c
+++ TCP/QB-before.c
@@ -7,27 +7,18 @@
 
 #define BUF_SIZE 1024
 #define PORT 9002
-
 struct message {
     int type;      // Type of message (e.g. 0 = login, 1 = logout, 2 = send message)
     int length;    // Length of payload in bytes
     char payload[1024];  // Payload data
 };
 
-int calculate_checksum(char* buf, int len) {
-    int sum = 0;
-    for (int i = 0; i < len; i++) {
-        sum += buf[i];
-    }
-    return sum;
-}
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]){
     int sockfd, connfd;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
     char buf[BUF_SIZE];
-    struct message msg;
 
     // Create a socket
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -45,14 +36,16 @@ int main(int argc, char* argv[]) {
     if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
-    } else {
-        printf("Binded to port %d\n", PORT); // 9001 doesnt work wtf?
+    }
+    else{
+        printf("Binded to port %d\n",PORT); // 9001 doesnt work wtf?
     }
 
     /* ---------- By this point the program is listening on the port (PORT value) --------- */
 
-    // Program will serve forever - until it's closed.
-    while (1) {
+    //Program will serve forever - until it's closed.
+    while(1)
+    {
         // Listen for incoming connections
         if (listen(sockfd, 5) < 0) {
             perror("listen failed");
@@ -66,29 +59,21 @@ int main(int argc, char* argv[]) {
             exit(EXIT_FAILURE);
         }
 
-        // Use read to read message received
-        int len = read(connfd, &msg, sizeof(msg));
+        // Use read to read message recieved
+        int len = read(connfd, buf, BUF_SIZE);
         if (len < 0) {
             perror("read failed");
             exit(EXIT_FAILURE);
         }
+        buf[len] = '\0';
 
-        /* --------- Message has been received now so now looking into the message type --------- */
+        /* --------- Message has been received now so now loking into the message type --------- */
+        printf("Received message: %s\n", buf);
+        
 
-        // Verify the message checksum
-        int expected_checksum = calculate_checksum((char*)&msg, sizeof(msg) - sizeof(int));
-        if (expected_checksum != msg.type) {
-            printf("Invalid message checksum\n");
-            send(connfd, "CHECKSUM FAILED", strlen("CHECKSUM FAILED"), 0);
-            continue;
-        }
-
-        // Print the message payload
-        printf("Received message of type %d, length %d: %s\n", msg.type, msg.length, msg.payload);
-
-        // Basic ACK, needs to acknowledge received and failed messages
+        // Basic ACK, needs to acknowledge recieved and failed messages
         char ack_msg[BUF_SIZE];
-        sprintf(ack_msg, "ACK for: '%s'", msg.payload);
+        sprintf(ack_msg, "ACK for: '%s'\n", buf);
         if (send(connfd, ack_msg, strlen(ack_msg), 0) < 0) {
             perror("send failed");
             exit(EXIT_FAILURE);
--- TCP/TM.py
+++ TCP/TM.py
@@ -1,8 +1,24 @@
 import socket
+import hashlib
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 QB_HOST = 'localhost'
-QB_PORT = 9001
+QB_PORT = 9002
+
+
+def build_message_with_header(message):
+    # Calculate the checksum of the message
+    message_checksum = hashlib.sha256(message.encode('utf-8')).hexdigest()
+
+    # Build the header, containing the message type and checksum
+    header = bytearray()
+    header.append(0x01) # message type 0x01 for example
+    header.extend(bytes.fromhex(message_checksum)) # 32-byte SHA-256 checksum
+
+    # Concatenate the header and message into a single byte array
+    message_with_header = header + message.encode('utf-8')
+    return message_with_header
+
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -10,9 +26,26 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         message_data = self.rfile.read(content_length)
         message = message_data.decode('utf-8')
 
-        # Send the data to the QB server using UDP
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.sendto(message_data, (QB_HOST, QB_PORT))
+        # Calculate the checksum of the message
+        message_checksum = hashlib.sha256(message_data).hexdigest()
+
+        # Build the header, containing the message type and checksum
+        header = bytearray()
+        header.append(0x01) # message type 0x01 for example
+        header.extend(bytes.fromhex(message_checksum)) # 32-byte SHA-256 checksum
+
+        # Concatenate the header and message into a single byte array
+        message_with_header = header + message_data
+
+        # Send the data to the QB server using TCP
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((QB_HOST, QB_PORT))
+            s.sendall(message_with_header)
+            print("Message Sent")
+
+            # Wait for the response from the server
+            response = s.recv(1024)
+            print("Response Received: ", response.decode())
 
         # Send a response to the client
         self.send_response(200)
@@ -24,8 +57,24 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    # Set up the HTTP server to listen on port 9000
-    server_address = ('', 9000)
-    httpd = HTTPServer(server_address, HTTPRequestHandler)
-    print('Listening on port 9000...')
-    httpd.serve_forever()
+    # Create a TCP socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # Connect to the QB server
+        s.connect((QB_HOST, QB_PORT))
+        print('Connected to QB server')
+
+        # Send messages to the QB server
+        while True:
+            message = input('Enter a message: ')
+            if message.lower() == 'q':
+                break
+
+            # Build message with header
+            message_with_header = build_message_with_header(message)
+
+            # Send message to the QB server
+            s.sendall(message_with_header)
+
+            # Receive response from the QB server
+            response = s.recv(1024)
+            print('Response from QB server:', response.decode('utf-8'))
