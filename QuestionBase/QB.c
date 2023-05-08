ced98159397befd66844d5ff73056db6830070fd
J Lumagbas <jadlumagbas@gmail.com>
2023-05-08 11:14:19 +0800

    Uses IP of the machine its running, not localhost

--- QuestionBase/QB.c
+++ QuestionBase/QB.c
@@ -5,6 +5,47 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <ifaddrs.h>
+#include <netdb.h>
+
+// Not DHCP, i doubt thats a requirement?
+// based on this https://www.sanfoundry.com/c-program-get-ip-address/
+// https://www.includehelp.com/c-programs/get-ip-address-in-linux.aspx
+void get_local_ip(char *ip, int size) {
+    struct ifaddrs *ifaddr, *ifa;
+    struct sockaddr_in *sa;
+    char host[NI_MAXHOST];
+    int found = 0;
+
+    // Get list of network interfaces and their associated addresses
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        exit(EXIT_FAILURE);
+    }
+    // Iterate over list of network interfaces
+    for (ifa = ifaddr; ifa != NULL && !found; ifa = ifa->ifa_next) {
+        // Retriee IPV4 addredd
+        if (ifa->ifa_addr != NULL && ifa->ifa_addr->sa_family == AF_INET) {
+            // Make sure that it doesnt use local host
+            sa = (struct sockaddr_in *) ifa->ifa_addr;
+            // inet_ntoa convert and return ip address in array format
+            if (strcmp(inet_ntoa(sa->sin_addr), "127.0.0.1") != 0) {
+               // Host name ip and address to buffer
+                if (getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in), host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST) == 0) {
+                    strncpy(ip, host, size); // store iP, host and size
+                    found = 1; // On successful find
+                }
+            }
+        }
+    }
+
+    freeifaddrs(ifaddr); // Free ifaddr memory
+    // If it fails to find the computer's IP address
+    if (!found) {
+        fprintf(stderr, "Unable to find local IP address\n");
+        exit(EXIT_FAILURE);
+    }
+}
 
 void create_socket(int *sockfd) {
     if ((*sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -15,20 +56,23 @@ void create_socket(int *sockfd) {
 
 void bind_socket(int sockfd) {
     struct sockaddr_in server_addr;
+    char ip[NI_MAXHOST];
+    int port = 9001;
 
+    // Get local IP address
+    get_local_ip(ip, sizeof(ip));
     // Initialize server address structure
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(9001);
-
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(port);
     // Bind the socket to a specific address and port
     if (bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
     else{
-        printf("Binded to port 9001\n");
+        printf("Server running on %s:%d\n", ip, port);
     }
 }
 
--- QuestionBase/QB.h
+++ QuestionBase/QB.h
@@ -8,5 +8,5 @@ void bind_socket(int sockfd);
 void listen_for_connections(int sockfd);
 void handle_connection(int sockfd);
 void close_connection(int connfd);
-
+void get_local_ip(char *ip, int size);
 #endif
